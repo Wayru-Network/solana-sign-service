22c9dce377b15404889376ebf379ad6988b3dc7c
david2899 <jironullad@gmail.com>
2025-05-20 08:51:05 -0600

    I added try catch

--- src/services/nfnodes/nfnodes.service.ts
+++ src/services/nfnodes/nfnodes.service.ts
@@ -1,9 +1,14 @@
 import db from "@/config/db";
 
 export const countNFNodesByWallet = async (userWallet: string): Promise<number> => {
-    const result = await db.query('SELECT id from nfnodes where wallet = $1', [userWallet]);
-    if (result.rows.length === 0) {
+    try {
+        const result = await db.query('SELECT id from nfnodes where wallet = $1', [userWallet]);
+        if (result.rows.length === 0) {
+            return 0;
+        }
+        return result.rows.length;
+    } catch (error) {
+        console.error('countNFNodesByWallet error: ', error);
         return 0;
     }
-    return result.rows.length;
 }
